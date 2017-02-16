4047d203f8084e6a71d4994af2770c83f16ddc2f
Patrick Yates <frozen.dinosaur@gmail.com>
2017-02-16 22:45:57 +1100

    Bump version number

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='plutokore',
     packages=find_packages(), 
-    version='0.3',
+    version='0.4',
     description='Python tool for analysing PLUTO simulation data',
     author='Patrick Yates',
     author_email='patrick.yates@utas.edu.au',
     url='https://github.com/opcon/plutokore',
     keywords=['pluto', 'astrophsyics'],
     license='MIT',
-    requires=['numpy', 'matplotlib', 'tabulate', 'astropy', 'h5py'],
+    requires=['numpy', 'matplotlib', 'tabulate', 'astropy', 'h5py', 'pyyaml'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-datafiles'],
     classifiers=[
