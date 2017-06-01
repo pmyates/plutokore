2414b24d4821536b6e307ab757c6d77f57d2afd8
Patrick Yates <patrick.yates@utas.edu.au>
2017-06-01 14:42:15 +1000

    [general] Update requirements
    * Added 'future' package to the requirements.
    * Bumped version to 0.10

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='plutokore',
     packages=find_packages(), 
-    version='0.9',
+    version='0.10',
     description='Python tool for analysing PLUTO simulation data',
     author='Patrick Yates',
     author_email='patrick.yates@utas.edu.au',
     url='https://github.com/pmyates/plutokore',
     keywords=['pluto', 'astrophsyics'],
     license='GPL-3.0',
-    requires=['numpy', 'matplotlib', 'tabulate', 'astropy', 'h5py', 'pyyaml', 'scipy'],
+    requires=['numpy', 'matplotlib', 'tabulate', 'astropy', 'h5py', 'pyyaml', 'scipy', 'contextlib2', 'future'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-datafiles'],
 )
