2bdab01d5c7de21cb421d700acd91d75376fcb05
Patrick Yates <patrick.yates@utas.edu.au>
2017-06-01 15:49:13 +1000

    [general] Fix setup.py not picking up requires correctly

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url='https://github.com/pmyates/plutokore',
     keywords=['pluto', 'astrophsyics'],
     license='GPL-3.0',
-    requires=['numpy', 'matplotlib', 'tabulate', 'astropy', 'h5py', 'pyyaml', 'scipy', 'contextlib2', 'future'],
+    install_requires=['numpy', 'matplotlib', 'tabulate', 'astropy', 'h5py', 'pyyaml', 'scipy', 'contextlib2', 'future'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-datafiles'],
 )
